6caeedb520a1c7fc1f678b3f3cfe140987cfd2ce
ThongHutech <2211>
2020-06-05 16:28:21 +0700

    xong tuan nay

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BigSchool.Models;
+using BigSchool.ViewModels;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+
+            return View();
+        }
+    }
+}
